de9432fd7a01a3891339e077d8a7def72ce1c9b7
Mansi Sharma <53896251+mansi35@users.noreply.github.com>
2023-01-28 14:14:27 +0530

    updated server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
-// const API = axios.create({ baseURL: 'https://memories-moments.herokuapp.com' });
+// const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memories-server-m754.onrender.com' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
